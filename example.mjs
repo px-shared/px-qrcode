99eac2e8050c58f257b0df9847c9188df2c40dc8
williampeterpaul <williampeterpaul@pm.me>
2022-08-10 21:22:45 +0100

    Code

---
+++ example.mjs
@@ -0,0 +1,31 @@
+import QRCode from './lib/index.js';
+
+const generateQR = async (text) => {
+  try {
+    const svg = await QRCode.toFile('example.svg', text, {
+      errorCorrectionLevel: 'H',
+      type: 'svg',
+      margin: 4,
+      format: {
+        background: 'white',
+        image: 'https://pngimg.com/uploads/fanta/fanta_PNG57.png',
+        data: {
+          shape: 'node',
+          gradient: ['orange', 'yellow']
+        },
+        inner: {
+          shape: 'kite',
+          gradient: ['orange', 'yellow']
+        },
+        outer: {
+          shape: 'kite',
+          gradient: ['orange', 'yellow']
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+generateQR('he');
